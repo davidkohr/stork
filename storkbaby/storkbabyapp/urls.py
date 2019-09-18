06fe9b280d5b8c8435fefef31b3b13f5d4d464cf
David Ko <david.ko@indexexchange.com>
2019-09-18 14:55:06 -0400

    Redirect to homepage improved

--- storkbaby/storkbabyapp/urls.py
+++ storkbaby/storkbabyapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^(?P<my_id>[0-9]+)/$', views.index, name='index'),
     url(r'^(?P<my_id>[0-9]+)/(?P<profile_id>[0-9]+)/profile/$', views.profile, name='profile'),
     url(r'^(?P<my_id>[0-9]+)/results/(?P<searched>[A-z0-9\+]+)$', views.results, name='results'),
