24f91d9672f923dac187f00d38a571e7c2911162
Emma Wilson <emma.wilson@indexexchange.com>
2019-09-18 10:55:02 -0400

    changes

--- storkbaby/storkbabyapp/urls.py
+++ storkbaby/storkbabyapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<profile_id>[0-9]+)/profile/$', views.results, name='results'),
+    url(r'^(?P<profile_id>[0-9]+)/profile/$', views.profile, name='profile'),
     url(r'^results/(?P<searched>[A-z0-9]+)$', views.results, name='search'),
 ]
