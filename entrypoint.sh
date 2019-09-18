8eeb7b421ddde919ae88b8f804d2cbbbf021c003
David Ko <david.ko@indexexchange.com>
2019-09-18 10:19:35 -0400

    Ignore sqlite3 file

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,11 @@ echo "Apply database migrations"
 python /code/storkbaby/manage.py makemigrations storkbabyapp
 python /code/storkbaby/manage.py migrate
 
+# Set create admin user
+echo "Creating super(admin) user"
+#python /code/storkbaby/manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@indexexchange.com', 'viper67')"
+python /code/storkbaby/manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(is_superuser=True); usr = User.objects.get(username='admin'); usr.set_password('viper123'); usr.save();"
+
 # Start server
 echo "Starting server"
 python /code/storkbaby/manage.py runserver 0.0.0.0:8080
