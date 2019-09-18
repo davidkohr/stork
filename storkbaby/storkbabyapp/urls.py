08380d6966a82c1adf6a8cf4a274bfd11e56abaa
David Ko <david.ko@indexexchange.com>
2019-09-18 15:51:43 -0400

    Logging in works

--- storkbaby/storkbabyapp/urls.py
+++ storkbaby/storkbabyapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^login/submit$', views.loginSubmit, name='loginSubmit'),
     url(r'^(?P<my_id>[0-9]+)/$', views.index, name='index'),
     url(r'^(?P<my_id>[0-9]+)/(?P<profile_id>[0-9]+)/profile/$', views.profile, name='profile'),
     url(r'^(?P<my_id>[0-9]+)/results/(?P<searched>[A-z0-9\+]+)$', views.results, name='results'),
