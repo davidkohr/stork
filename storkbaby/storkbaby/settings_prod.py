83bd0360d9d3fe250605c83451eb3faadb06a0f0
baargav.raghavan <baargav.raghavan@indexexchange.com>
2019-12-04 23:25:59 -0500

    run in debug?

--- storkbaby/storkbaby/settings_prod.py
+++ storkbaby/storkbaby/settings_prod.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.33.10', '127.0.0.1', 'foobaar-stork.herokuapp.com']
 
